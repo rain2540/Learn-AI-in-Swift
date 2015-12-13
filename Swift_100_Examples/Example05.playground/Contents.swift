b3cfb192f1f9d197ffdc5a05145bcf1a4c9f9e59
rain2540 <rain2540@foxmail.com>
2015-12-13 16:32:16 +0800

    Add Example 05

---
+++ Swift_100_Examples/Example05.playground/Contents.swift
@@ -0,0 +1,25 @@
+//: Example 05
+//:
+//: 题目：输出九九乘法表。
+//:
+//: 1.程序分析：分行与列考虑，共9行9列，i控制行，j控制列。
+//:
+//: 2.程序源代码：
+
+import Foundation
+
+//  extension to Int
+extension Int {
+    /// 格式化输出
+    public func format(f: String) -> String {
+        return String(format: "%\(f)d", self)
+    }
+}
+
+for i in 1 ..< 10 {
+    for j in 1 ... i {
+        var result = i * j
+        print("\(i) * \(j) = " + result.format("-2"), terminator: "  ")
+    }
+    print("\n")
+}
