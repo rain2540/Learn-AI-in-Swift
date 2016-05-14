23028ef16d89b13ccb6c2469ea681eb676677d8c
Rain <rain2540@foxmail.com>
2016-05-14 13:03:44 +0800

    Update Example03

--- Swift_100_Examples/Example03.playground/Contents.swift
+++ Swift_100_Examples/Example03.playground/Contents.swift
@@ -35,6 +35,6 @@ if year % 400 == 0 || (year % 4 == 0 && year % 100 != 0) {  /*判断是不是闰
     leap = 0
 }
 if leap == 1 && month > 2 { /*如果是闰年且月份大于2,总天数应该加一天*/
-    sum++
+    sum += 1
 }
 print("It is the \(sum)th day.")
