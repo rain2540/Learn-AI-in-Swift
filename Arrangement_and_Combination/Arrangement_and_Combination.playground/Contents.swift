12db04fe2a8a50ef5eef25e74733e310f2a6dca1
Rain <rain2540@foxmail.com>
2019-05-12 14:42:52 +0800

    feat (A & C) : throw error

--- Arrangement_and_Combination/Arrangement_and_Combination.playground/Contents.swift
+++ Arrangement_and_Combination/Arrangement_and_Combination.playground/Contents.swift
@@ -1,9 +1,12 @@
 import UIKit
 
 enum ACError: Error {
-    case FactorialOfNegativeNumbers
+    case factorialOfNegativeNumbers
 }
 
-func factorial(of num: Int) -> Int {
+func factorial(of num: Int) throws -> Int {
+    guard num >= 0 else {
+        throw ACError.factorialOfNegativeNumbers
+    }
     return num > 0 ? (1 ... num).reduce(1, { $0 * $1 }) : 1
 }
