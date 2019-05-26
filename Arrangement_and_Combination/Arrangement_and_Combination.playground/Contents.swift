82cb64a3149f107864e06b94b4465ed5bd2d65a4
Rain <rain2540@foxmail.com>
2019-05-26 08:28:15 +0800

    feat (A & C) : add an example

--- Arrangement_and_Combination/Arrangement_and_Combination.playground/Contents.swift
+++ Arrangement_and_Combination/Arrangement_and_Combination.playground/Contents.swift
@@ -6,6 +6,7 @@ enum ACError: Error {
     case combinationParameterNonCompliance
 }
 
+
 func factorial(of num: Int) throws -> Int {
     guard num >= 0 else {
         throw ACError.factorialOfNegativeNumbers
@@ -13,21 +14,28 @@ func factorial(of num: Int) throws -> Int {
     return num > 0 ? (1 ... num).reduce(1, { $0 * $1 }) : 1
 }
 
-let n = 4, m = 3
+func arrangement(n: Int, m: Int) throws -> Int {
+    guard m <= n else {
+        throw ACError.arrangementParameterNonCompliance
+    }
+    return try factorial(of: n) / factorial(of: n - m)
+}
+
+func combination(n: Int, m: Int) throws -> Int {
+    guard m <= n else {
+        throw ACError.combinationParameterNonCompliance
+    }
+    return try factorial(of: n) / (factorial(of: m) * factorial(of: n - m))
+}
+
+
+let n = 4, m = 2
 
 do {
     let res = try factorial(of: n)
     print("factorial of \(n) is", res)
 } catch {
     print(error)
-}
-
-
-func arrangement(n: Int, m: Int) throws -> Int {
-    guard m <= n else {
-        throw ACError.arrangementParameterNonCompliance
-    }
-    return try factorial(of: n) / factorial(of: n - m)
 }
 
 do {
@@ -37,10 +45,9 @@ do {
     print(error)
 }
 
-
-func combination(n: Int, m: Int) throws -> Int {
-    guard m <= n else {
-        throw ACError.combinationParameterNonCompliance
-    }
-    return try factorial(of: n) / (factorial(of: m) * factorial(of: n - m))
+do {
+    let res = try combination(n: n, m: m)
+    print("result of C(\(n), \(m)) is \(res)")
+} catch {
+    print(error)
 }
