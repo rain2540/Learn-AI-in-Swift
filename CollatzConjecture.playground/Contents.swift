dc5051c160e99d8964d357d6c397fe66ae994109
Rain2540 <rain2540@foxmail.com>
2020-04-14 23:55:53 +0800

    docs (Collatz Conjecture) : add comment

--- CollatzConjecture.playground/Contents.swift
+++ CollatzConjecture.playground/Contents.swift
@@ -1,5 +1,9 @@
 import UIKit
 
+/*:
+ 考拉兹猜想：一个正整数，如果它是奇数，则对它乘3再加1，如果它是偶数，则对它除以2，如此循环，最终都能够得到1
+ */
+
 typealias CollatzInfo = (number: Int, step: Int, path: [Int])
 
 func collatzConjecture(_ number: Int) -> CollatzInfo {
